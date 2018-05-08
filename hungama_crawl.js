6805dc214b08c0e0a82e6690777c34c5e3a8eaa8
Sourav De <srvz39@gmail.com>
2018-05-09 00:54:47 +0530

    new logic for whole site crawling

---
+++ hungama_crawl.js
@@ -0,0 +1,50 @@
+var Crawler = require("crawler");
+var _ = require('lodash')
+
+links = [], visited = [],t=0;
+var c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: function (error, res, done) {
+        if (error) {
+            console.log(error);
+        } else {
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            //console.log($('a').attr('href'));
+            $('a').each(function () {
+                var text = $(this).text();
+                var link = $(this).attr('href');
+                if (_.includes(link, 'java') == false  && _.includes(link,'hungama'))
+                    links.push(link)
+            })
+            temp = _.differenceWith(_.uniq(links), visited);
+            if(temp.length>0){
+                links.push(temp)
+            console.log(temp)}
+            t++
+            console.log('Attempt : '+ t +' Total links: ', _.uniq(links).length)
+            for (i = 0; i < links.length; i++) {
+                if (_.includes(visited, links[i]) == false) {
+                    visited.push(links[i])
+                    visited = _.uniq(visited)
+                    c.queue(links[i])
+                }
+            }
+        }
+        done();
+    }
+});
+
+// c.on('schedule', function (options) {
+//     options.proxy = "http://proxy.intra.bt.com:8080";
+// });
+// Queue just one URL, with default callback
+c.queue("http://www.hungama.com");
+
+// Queue a list of URLs
+//c.queue(["http://www.google.com/", "http://www.yahoo.com"]);
+// for (i=0;i<links.length;i++){
+//     c.queue(links[i])
+// }
