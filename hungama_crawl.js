f2da7515b979623aa265d3bff16e887533abe43b
Sourav De <srvz39@gmail.com>
2018-05-09 23:38:28 +0530

    local commit

--- hungama_crawl.js
+++ hungama_crawl.js
@@ -1,7 +1,10 @@
-var Crawler = require("crawler");
+var Crawler = require("crawler")
 var _ = require('lodash')
 
-links = [], visited = [],t=0;
+links = [], visited = [],i = 0,t=0, currentUrl='';
+
+
+
 var c = new Crawler({
     maxConnections: 10,
     // This will be called for each crawled page
@@ -24,11 +27,12 @@ var c = new Crawler({
                 links.push(temp)
             console.log(temp)}
             t++
-            console.log('Attempt : '+ t +' Total links: ', _.uniq(links).length)
-            for (i = 0; i < links.length; i++) {
+            console.log('Attempt : '+ t +' Current url: '+ links[i] +' Total links: ', _.uniq(links).length)
+            for (; i < links.length; i++) {
                 if (_.includes(visited, links[i]) == false) {
                     visited.push(links[i])
                     visited = _.uniq(visited)
+                    currentUrl = links[i]
                     c.queue(links[i])
                 }
             }
