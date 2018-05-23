81eecab3f823ffd6fff4213966e5cbbea6e40611
SrvZ <srvz39@gmail.com>
2018-05-23 11:03:09 +0530

    hooq tv added

---
+++ Hooq/hooq_tv_details.js
@@ -0,0 +1,115 @@
+var fs = require("fs");
+var _ = require("lodash");
+
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({
+    show: true,
+    //cookiesFiles: "./cookie.txt"
+    webPreferences: {
+        partition: 'persist: testing'
+    }
+});
+vo = require('vo'),
+    nightmare2 = Nightmare();
+
+
+cookieJar = JSON.parse(fs.readFileSync('hooq_nmjs_cookie.json', 'utf8'))
+
+console.log(cookieJar)
+
+
+
+url = "https://www.hooq.tv/catalog/073aeadb-6a8a-4743-8ac5-64903a84e94f?__sr=feed&amp";
+
+
+
+url = "https://www.hooq.tv/verify/email?h=eyJvdHAiOiI1NTE0NjgiLCJoYXNoIjoiM2I2ZUlja2hzTkZsQnpPVFUwWGVSWHArWldVPSIsInNpZ25hdHVyZSI6IjE0MzVjM2QwMGY4ODM1ODEwYjhjMjc2NjE1ZWYyMmFjZDhjOTI0NmI3N2QxOTM3MWYxZmFmMDIyMDkwNTJhZGMiLCJlbWFpbCI6ImhvZ2F5b21lcGlAYjJieC5uZXQiLCJjb3VudHJ5IjoiSU4iLCJhY3Rpb24iOiJzaWdudXAifQ=="
+url =
+    "https://www.hooq.tv/catalog/073aeadb-6a8a-4743-8ac5-64903a84e94f?__sr=feed&amp"
+
+jsonUrl = []
+for (i = 0; i < 15; i++) {
+    jsonUrl[i] = "https://www.hooq.tv/search?page=" + (i + 1) + "&size=18&scope=min%2Cimages&as=TVSHOW";
+}
+
+//https://www.hooq.tv/search?page=2&size=18&scope=min%2Cimages&as=TVSHOW
+//hifa@aditus.info
+//  url ="https://www.hooq.tv/verify/email?h=eyJvdHAiOiIwMDk4NTEiLCJoYXNoIjoiWnhJNWhWVzRQaFM1UVNGU0pTMXZFVHFBQWEwPSIsInNpZ25hdHVyZSI6IjAyMWU0MDU0MDliNmVhMzAwNzY5MDgyMjhiYjhhY2Q3MzVlYjU1ZDUzZTJiODI2ZGM0YmQ3MmE2NDVjMTRjNWQiLCJlbWFpbCI6ImRpYmVmb3dpc29Ac3RvcmlxYXguY29tIiwiY291bnRyeSI6IklOIiwiYWN0aW9uIjoic2lnbmluIn0=";
+// nightmare
+//   .useragent(
+//     "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+//   .goto(url)
+//   //.cookies.get()
+//   //.wait(".btn")
+//   //.click('.btn')
+//   //.wait("#email")
+//   //.type("#email", "dibefowiso@storiqax.com")
+//   //.click('#submit-button')
+//   .wait(2000)
+//   //.goto('https://www.hooq.tv/search?page=2&size=18&scope=min%2Cimages&as=MOVIE')
+//   .then(() => {
+//     // console.log(cookies)
+//     // fs.writeFileSync('hooq_nmjs_cookie',JSON.stringify(cookies))
+
+//     var run = function*() {
+//       //var jsonUrl = ['http://www.yahoo.com', 'http://example.com', 'http://w3c.org'];
+//       var response = [];
+//       jsonUrl = [];
+//       for (i = 0; i < 144; i++) {
+//         jsonUrl[i] = "https://www.hooq.tv/search?page=" + (i+1) + "&size=18&scope=min%2Cimages&as=MOVIE";
+//       }
+//       for (var i = 0; i < jsonUrl.length; i++) {
+//         var title = yield nightmare
+//           .goto(jsonUrl[i])
+//           .wait("body")
+//           .evaluate(() => {
+//             response.push(body);
+//             fs.appendFileSync("hooq_movie_dump.json", JSON.stringify(response));
+//           });
+//         //response.push(body);
+//       }
+//       return response;
+//     };
+
+//     vo(run)(function(err, res) {
+//       console.dir(res);
+//     });
+//   });
+
+
+//https://www.hooq.tv/search?page=2&size=18&scope=min%2Cimages&as=TVSHOW
+async function main() {
+    response = []
+    for (i = 0; i < 15; i++) {
+        jsonUrl[i] = "https://www.hooq.tv/search?page=" + (i + 1) + "&size=18&scope=min%2Cimages&as=TVSHOW";
+    }
+
+    //   var nightmare = Nightmare({ show: true })
+
+    for (let i = 0; i < jsonUrl.length; i++) {
+        const url = jsonUrl[i];
+        const title = await nightmare
+            .useragent(
+                "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+            .goto(url)
+            .wait('body')
+            .evaluate(() => {
+                response = [];
+                response.push(document.body.innerText);
+                return response
+            }).then(result => {
+                data = JSON.parse(result).data
+                
+                    response.push(data);
+                                
+                fs.appendFileSync("hooq_tv_dump3.json",JSON.stringify(data))
+                console.log(url);
+            })
+
+        //console.log(url);
+    }
+    fs.writeFileSync("hooq_tv_test.json",JSON.stringify(response));
+    await nightmare.end()
+}
+
+main().catch(console.error)
--- Hooq/movie_cast_n_genre.js
+++ Hooq/movie_cast_n_genre.js
@@ -1,4 +1,4 @@
-const fs =require('fs')
+const fs = require('fs')
 const Nightmare = require("nightmare");
 const nightmare = Nightmare({
   show: false,
@@ -14,8 +14,8 @@ var obj = JSON.parse(fs.readFileSync("hooq_movies.json"));
 
 movieURLs = []
 
-for(i=0;i<obj.length;i++){
-    movieURLs[i] = obj[i].url
+for (i = 0; i < obj.length; i++) {
+  movieURLs[i] = obj[i].url
 }
 
 console.log(movieURLs)
@@ -23,17 +23,17 @@ console.log(movieURLs)
 response = []
 async function main() {
   response = [];
-//   for (i = 0; i < 144; i++) {
-//     jsonUrl[i] =
-//       "https://www.hooq.tv/search?page=" +
-//       (i + 1) +
-//       "&size=18&scope=min%2Cimages&as=MOVIE";
-//   }
+  //   for (i = 0; i < 144; i++) {
+  //     jsonUrl[i] =
+  //       "https://www.hooq.tv/search?page=" +
+  //       (i + 1) +
+  //       "&size=18&scope=min%2Cimages&as=MOVIE";
+  //   }
 
   //   var nightmare = Nightmare({ show: true })
 
   for (let i = 0; i < movieURLs.length; i++) {
-    const url = movieURLs[i]//.replace(';autoplay=1','');
+    const url = movieURLs[i] //.replace(';autoplay=1','');
     const title = await nightmare
       .useragent(
         "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
@@ -41,19 +41,25 @@ async function main() {
       .goto(url)
       .wait("body")
       .evaluate(() => {
-        data =[]
+        data = []
+        stars ='',director='',genre='',lang='';
+
+        if(document.querySelectorAll("div.tr:nth-child(5) > div:nth-child(2)").length>0)
+          stars = document.querySelector("div.tr:nth-child(5) > div:nth-child(2)").innerText
+        if(document.querySelectorAll("div.tr:nth-child(4) > div:nth-child(2)").length>0)
+          director = document.querySelector("div.tr:nth-child(4) > div:nth-child(2)").innerText
+        if(document.querySelectorAll("div.title-links:nth-child(4)").length>0)
+          genre = document.querySelector("div.title-links:nth-child(4)").innerText
+        if(document.querySelectorAll('#card-description > div > div.tbl > div:nth-child(2) > div:nth-child(2)').length>0)
+          lang = document.querySelector('#card-description > div > div.tbl > div:nth-child(2) > div:nth-child(2)').innerText
         data.push({
-          url: document.URL,
-          stars: document.querySelector(
-            "div.tr:nth-child(5) > div:nth-child(2)"
-          ).innerText,
-          director: document.querySelector(
-            "div.tr:nth-child(4) > div:nth-child(2)"
-          ).innerText,
-          genre: document.querySelector("div.title-links:nth-child(4)")
-            .innerText
+          url: document.URL,          
+          stars: stars,
+          director: director,
+          genre: genre,
+          lang: lang
         });
-        
+
         return data
       })
       .then(result => {
@@ -63,6 +69,10 @@ async function main() {
 
         fs.appendFileSync("hooq_movie_castngenre.json", JSON.stringify(result));
         console.log(url);
+      })
+      .catch(error => {
+        console.error("Failed:", error);
+
       });
 
     //console.log(url);
