d46a75f68ac96cfcba1c4ba97b7d546eba1b996e
SrvZ <srvz39@gmail.com>
2018-08-20 15:14:46 +0530

    crackel almost complete

--- crackle/series_cast.js
+++ crackle/series_cast.js
@@ -33,7 +33,7 @@ castURLS = []
 // }
 
 // console.log(seriesLinkIDS)
-
+jsonData =[]
 for (i = 0; i < seriesDetails.length; i++) {
     castURLS[i] = seriesDetails[i].castURL
 }
@@ -57,8 +57,8 @@ for (i = 0; i < seriesDetails.length; i++) {
 
     //     return data
     // })
-    // for (i = 0; i < castURLS.length; i++) {
-        for (i = 0; i < 20; i++) {
+    for (i = 0; i < castURLS.length; i++) {
+        // for (i = 0; i < 20; i++) {
 
         
         castURL = castURLS[i]
@@ -85,15 +85,36 @@ for (i = 0; i < seriesDetails.length; i++) {
 
         })
         console.log(castDetails.cast)
-        seriesDetails[i].cast = castDetails.cast
-        seriesDetails[i].directors = castDetails.directors
-        delete seriesDetails[i].seriesID
-        delete seriesDetails[i].episodeID
-        delete seriesDetails[i].castURL
+        // seriesDetails[i].stars = castDetails.cast
+        // seriesDetails[i].directors = castDetails.directors
+        // delete seriesDetails[i].seriesID
+        // delete seriesDetails[i].episodeID
+        // delete seriesDetails[i].castURL
+
+        jsonData.push({
+            
+            series_name: seriesDetails[i].series_name, // String | For anime name,
+            title: seriesDetails[i].title, //String | For episode title,
+            // language: language, //String | For language of this anime,
+            episode_number: seriesDetails[i].episode_number, //Integer | For episode number
+            season_name: seriesDetails[i].season_name, //String | Should be formatted like Season 1, Season 2.
+            release_date_formatted: seriesDetails[i].release_date_formatted, //String | If full date is available then only this parameter should be used. Format - %d-%B-%Y - for ex: 17-May-2018, 23-November-2018, 02-December-2018.
+            release_year: seriesDetails[i].release_year, //Integer | Many times only release year is present. Should be only used if only release year data is available and not full date. Either use release_year or release_date_formatted.
+            synopsis: seriesDetails[i].synopsis, //String | Episode synopsis
+            link: seriesDetails[i].link, //String | Episode link
+            series_link: seriesDetails[i].series_link, //String | If anime link is different then episode link.
+            video_length: seriesDetails[i].video_length, //Integer | In seconds. Always convert the episode length to time in seconds.
+            image_link: seriesDetails[i].image_link, //String | episode image link.
+            stars: castDetails.cast, //Array of Dictionary: [{"name": "Actor name", "image_link": "Actor image link if available"}].
+            directors: castDetails.directors //Array of string | If multiple director names available.
+            // director: director //String | If only one director name is available.
+
+        })
+
     }
 
 
 
-    fs.writeFileSync('test.json', JSON.stringify(seriesDetails, null, 2))
+    fs.writeFileSync('test.json', JSON.stringify(jsonData, null, 2))
     await browser.close();
 })();
